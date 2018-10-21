d5db2596a8131d11560085f2d13736a36e872d5a
Benjamin Mintz <bmintz@protonmail.com>
2018-10-21 07:23:17 +0000

    schema: add a check to blocks

--- schema.sql
+++ schema.sql
@@ -1,15 +1,19 @@
 CREATE TABLE IF NOT EXISTS highlights(
-	"user" BIGINT NOT NULL,
 	guild BIGINT NOT NULL,
+	"user" BIGINT NOT NULL,
 	highlights TEXT[] NOT NULL,
 
-	PRIMARY KEY ("user", guild));
+	PRIMARY KEY (guild, "user"));
 
 CREATE TABLE IF NOT EXISTS blocks(
-	"user" BIGINT,
 	guild BIGINT NOT NULL,
+	"user" BIGINT,
 	categories BIGINT[],
 	channels BIGINT[],
 	users BIGINT[],
 
-	PRIMARY KEY ("user", guild));
+	PRIMARY KEY (guild, "user"),
+	CHECK (
+		categories IS NOT NULL
+		OR channels IS NOT NULL
+		OR users IS NOT NULL));
