41da38be4d8748ddee67c25e319711709ddc36f2
Benjamin Mintz <bmintz@protonmail.com>
2018-10-22 05:30:20 +0000

    schema: stop caring about the type of blocked entity

--- schema.sql
+++ schema.sql
@@ -6,15 +6,7 @@ CREATE TABLE IF NOT EXISTS highlights(
 CREATE UNIQUE INDEX IF NOT EXISTS highlights_guild_user_highlight_unique_idx ON highlights (guild, "user", LOWER(highlight));
 
 CREATE TABLE IF NOT EXISTS blocks(
-	guild BIGINT NOT NULL,
-	"user" BIGINT,
-	category BIGINT,
-	channel BIGINT,
-	blocked_user BIGINT,
+	"user" BIGINT NOT NULL,
+	entity BIGINT NOT NULL);
 
-	CONSTRAINT something_is_blocked CHECK (
-		category IS NOT NULL
-		OR channel IS NOT NULL
-		OR blocked_user IS NOT NULL));
-
-CREATE INDEX IF NOT EXISTS blocks_guild_user_idx ON blocks (guild, "user");
+CREATE UNIQUE INDEX IF NOT EXISTS blocks_unique_idx ON blocks ("user", entity);
