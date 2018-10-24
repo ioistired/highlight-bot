357ef82282ec72e3e3738834373d05c66690b7fd
bmintz <bmintz@protonmail.com>
2018-10-24 20:02:52 +0000

    document cogs/highlight.notification_message

--- cogs/highlight.py
+++ cogs/highlight.py
@@ -277,6 +277,12 @@ class Highlight:
 
 	@classmethod
 	async def notification_message(cls, user, highlight, message):
+		"""Create an embed message to send to the user for being highlighted
+
+		Here's what it looks like:
+		https://dingo.csdisaster.club/~ben/highlight-notification-example.png
+		"""
+
 		content = (
 			f'In {message.channel.mention} for server {message.guild.name}, '
 			f'you were mentioned with highlight word **{highlight}**')
