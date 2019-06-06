49b73fd5b59efd4db217fbb4c1064c5a806caa2f
Benjamin Mintz <bmintz@protonmail.com>
2019-06-06 23:20:56 +0000

    utils.asyncexecutor was not unused after all
    This reverts commit 9877e56943846a4d5d74dd88608dfbaf26758b88.

--- utils.py
+++ utils.py
@@ -49,6 +49,23 @@ class LRUDict(collections.OrderedDict):
 
 		super().__setitem__(key, value)
 
+def asyncexecutor(*, timeout=None, loop=None, executor=None):
+	"""decorator that turns a synchronous function into an async one
+
+	Created by @Arqm#9302 (ID 325012556940836864). XXX Unknown license
+	"""
+	def decorator(func):
+		@functools.wraps(func)
+		def wrapper(*args, **kwargs):
+			partial = functools.partial(func, *args, **kwargs)
+			loop_ = loop or asyncio.get_event_loop()
+
+			coro = loop_.run_in_executor(executor, partial)
+			# don't need to check if timeout is None since wait_for will just "block" in that case anyway
+			return asyncio.wait_for(coro, timeout=timeout, loop=loop_)
+		return wrapper
+	return decorator
+
 class Guild(commands.Converter):
 	@staticmethod
 	async def convert(context, argument):
