c0f748657e1fe246182fe7a724284cf835c822c2
Benjamin Mintz <bmintz@protonmail.com>
2019-06-07 05:34:35 +0000

    schema: add some indexes

--- schema.sql
+++ schema.sql
@@ -3,10 +3,12 @@ CREATE TABLE IF NOT EXISTS highlights(
 	"user" BIGINT NOT NULL,
 	highlight TEXT NOT NULL);
 
-CREATE UNIQUE INDEX IF NOT EXISTS highlights_guild_user_highlight_unique_idx ON highlights (guild, "user", LOWER(highlight));
+CREATE UNIQUE INDEX IF NOT EXISTS highlights_uniq_idx ON highlights (guild, "user", LOWER(highlight));
+CREATE INDEX IF NOT EXISTS highlights_guild_idx ON highlights (guild);
 
 CREATE TABLE IF NOT EXISTS blocks(
 	"user" BIGINT NOT NULL,
 	entity BIGINT NOT NULL);
 
 CREATE UNIQUE INDEX IF NOT EXISTS blocks_unique_idx ON blocks ("user", entity);
+CREATE INDEX IF NOT EXISTS blocks_entity_idx ON blocks (entity);
